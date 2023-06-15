fa85ed7d0d2fb8c7459bd4073fe00b2bee0df4d4
lance <lanceparker2001@gmail.com>
2023-06-15 11:57:47 +0530

    Added Footer and ErrorPage section

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Button } from './styles/Button';
+import { NavLink } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <Wrapper>
+      <div className='container'>
+        <div>
+          <h2>404</h2>
+          <h2>Uh Oh! You're lost.</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A repudiandae voluptatem ratione iure eaque deserunt ipsa voluptatibus laboriosam veniam impedit!</p>
+          <Button>
+            <NavLink to="/">Go back to Home</NavLink>
+          </Button>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  .container {
+    padding: 9rem 0;
+    text-align: center;
+
+    h2 {
+      font-size: 10rem;
+    }
+
+    h3 {
+      font-size: 4.2rem;
+    }
+
+    p {
+      margin: 2rem 0;
+    }
+  }
+`;
+
 
 export default ErrorPage
--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Trusted = () => {
   return (
-    <Wrapper className='brand-section'>
-      <h3>Trusted By 1000+ Companies</h3>
-      <div className="brand-section-slider">
-        {/* my iamges  */}
-        <div className="slide">
+    <Wrapper className="brand-section">
+      <div className="container">
+        <h3>Trusted By 1000+ Companies</h3>
+        <div className="brand-section-slider">
+          {/* my 1st img  */}
+          <div className="slide">
             <img
               src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
               alt="trusted-brands"
@@ -36,14 +37,16 @@ const Trusted = () => {
               alt="trusted-brands"
             />
           </div>
+        </div>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   padding: 9rem 0;
   background-color: ${({ theme }) => theme.colors.bg};
+
   .brand-section {
     padding: 12rem 0 0 0;
   }
@@ -54,10 +57,12 @@ const Wrapper = styled.section`
     font-size: 2rem;
     font-weight: bold;
   }
+
   img {
     min-width: 10rem;
     height: 10rem;
   }
+
   .brand-section-slider {
     margin-top: 3.2rem;
     display: flex;
@@ -65,6 +70,7 @@ const Wrapper = styled.section`
     align-items: center;
     flex-direction: row;
   }
+
   @media (max-width: ${({ theme }) => theme.media.mobile}) {
     .brand-section-slider {
       margin-top: 3.2rem;
@@ -76,4 +82,4 @@ const Wrapper = styled.section`
   }
 `;
 
-export default Trusted
+export default Trusted;
