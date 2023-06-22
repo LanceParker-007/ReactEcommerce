cf079b0178db52abf92f324d3b23e8205baf11e7
lance <lanceparker2001@gmail.com>
2023-06-22 15:57:02 +0530

    Created CartItem component

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,9 +1,29 @@
 import styled from "styled-components";
+import { useCartContext } from "./context/cart_context";
+import CartItem from "./components/CartComponent/CartItem";
 
 const Cart = () => {
-  return <Wrapper>
-    Cart
-  </Wrapper>;
+  const { cart } = useCartContext();
+  // console.log(cart);
+  return (
+    <Wrapper>
+      <div className="container">
+        <div className="card_heading grid grid-five-column">
+          <p>Item</p>
+          <p className="cart-hide">Price</p>
+          <p>Quantity</p>
+          <p className="cart-hide">Subtotal</p>
+          <p>Remove</p>
+        </div>
+        <hr />
+        <div className="cart-item">
+          {cart.map((curElem) => {
+            return <CartItem key={curElem.id} {...curElem} />;
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,7 +1,23 @@
 const cartReducer = (state, action) => {
   if (action.type === "ADD_TO_CART") {
     let { id, color, amount, product } = action.payload;
-    console.log("Add to cart Reducer", product);
+    console.log("Add to cart Reducer", product.image[0]);
+    let cartProduct;
+
+    cartProduct = {
+      id: id + color,
+      name: product.name,
+      color,
+      amount,
+      image: product.image[0],
+      price: product.price,
+      max: product.stock,
+    };
+
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
   }
 
   return state; // it must be return
